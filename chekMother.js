0b6ec30203edbca48c0bf44d0e74672d3585c827
Eugene Pokidko <42346779+Pievy@users.noreply.github.com>
2019-03-06 17:02:42 +0500

    index, style, checkMother
    ////////
    index:
    • Две кнопки слились в одну
    ////////
    checkMother:
    • Маленькие исправления по вложенности
    • Добавлен метод проверяющий на то что введено.
    (Ссылка или файл?)

--- chekMother.js
+++ chekMother.js
@@ -11,24 +11,50 @@ jQuery.getJSON(adress, function(data){
 
     output+="</ul>";
     document.getElementById("out").innerHTML=output;
-} );
+});
 };
 function getJN()
 {
     var request ="https://github.com/Contrkultya/nardy_MOORA/blob/master/pedofile.json";
-   var file = new XMLHttpRequest();
-   file.open('GET', request);
-   file.responseType='json';
-   file.send();
-   file.onload=function(){
-       var bivshaya = file.response;
-       populateHeader(bivshaya);
-       showBivshaya(bivshaya);
-}
-    function populateHeader(walle)
-    {
+    var file = new XMLHttpRequest();
+    file.open('GET', request);
+    file.responseType='json';
+    file.send();
+    file.onload=function(){
+        var bivshaya = file.response;
+        populateHeader(bivshaya);
+        showBivshaya(bivshaya);
+    }
+    function populateHeader(walle){
         document.getElementById("out").innerHTML=name;
     }
 
 
+}
+
+var boolExile = false, boolFile = false;
+function file_or_exile_Checker(X){
+    if (X == 1)
+    {
+        boolFile = true;
+        boolExile = false;
+        document.getElementById('siteURL').value = '';
+    }
+    else 
+    {
+        boolExile = true;
+        boolFile = false;
+        document.getElementById('fileLocation').value = '';
+    }
+
+}
+function file_or_exile(){
+    if (boolFile)
+    {
+        //do some method for File
+    }
+    else if (boolExile)
+    {
+        //do some method for Exile
+    }
 }

90f53c08e9c016adf05b7e0ec19a15b404bbecb7
Sergo Skeletonson <40607257+Contrkultya@users.noreply.github.com>
2019-03-06 13:55:17 +0500

    Add files via upload
    WEll, lets try it out

---
+++ chekMother.js
@@ -0,0 +1,9 @@
+// install slim version of 
+adress = document.GetElementById("Timofey, input ID HERE").value;
+
+jQuery.getJSON( adress, gotit(data){
+	// input your output function here
+	// yeah, just fuckin copy it
+	// govnokod, suka blyat
+	//subscribe to pewdiepie
+} );

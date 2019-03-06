c666996226b5f51b6c4ff3cbdb799432ffdf4c56
Igor Krupkin <48021172+Cuberian@users.noreply.github.com>
2019-03-06 15:31:36 +0500

    Update chekMother.js

--- chekMother.js
+++ chekMother.js
@@ -1,9 +1,34 @@
 // install slim version of 
-adress = document.GetElementById("fileLocation").value;
+function checkMother()
+{
+var adress = document.getElementById("siteURL").value;
 
-jQuery.getJSON( adress, gotit(data){
-	// input your output function here
-	// yeah, just fuckin copy it
-	// govnokod, suka blyat
-	//subscribe to pewdiepie
+jQuery.getJSON(adress, function(data){
+    var output="<ul>";
+    for (var i in data.name) {
+        output+="<li>" + data.name[i]+"</li>";
+    }
+
+    output+="</ul>";
+    document.getElementById("out").innerHTML=output;
 } );
+};
+function getJN()
+{
+    var request ="https://github.com/Contrkultya/nardy_MOORA/blob/master/pedofile.json";
+   var file = new XMLHttpRequest();
+   file.open('GET', request);
+   file.responseType='json';
+   file.send();
+   file.onload=function(){
+       var bivshaya = file.response;
+       populateHeader(bivshaya);
+       showBivshaya(bivshaya);
+}
+    function populateHeader(walle)
+    {
+        document.getElementById("out").innerHTML=name;
+    }
+
+
+}

c45c212b13ef7e9ba5db8159aab42cc02e8764f4
Igor Krupkin <48021172+Cuberian@users.noreply.github.com>
2019-03-09 23:44:14 +0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,22 @@
+function checkMother() {
+    var date = '[{ "name": "Доска 1", "children": [{ "name": "Список задач 1.1","children": [{ "name": "Задача 1.1.1" },{ "name": "Задача 1.1.2" }]},{"name": "Список задач 1.2","children": [{ "name": "Задача 1.2.1" },{ "name": "Задача 1.2.2" }]},{"name": "Список задач 1.3"}]},{"name": "Доска 2"}]';
+    dat = JSON.parse(date);
+    var html;
+     function rec(data)
+    {
+    html += "<ul>";
+      data.forEach(element => {
+          html+="<li>";
+          html+=element.name;
+          if(element.children)
+          {
+            rec(element.children);
+          }
+          html+="</li>";
+      }); 
+      html += "</ul>";
+      return html;
+    } 
+    cool=rec(dat); 
+    document.getElementById('out').innerHTML = cool;
+}

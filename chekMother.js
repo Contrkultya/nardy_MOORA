272a30f98ea6e3b7147177fbb27361e63a8c88fe
Eugene Pokidko <42346779+Pievy@users.noreply.github.com>
2019-03-09 18:15:32 +0500

    index, chekMother, style
    за работу

--- chekMother.js
+++ chekMother.js
@@ -1,69 +1,10 @@
-// install slim version of 
-function checkMother()
-{
-var adress = document.getElementById("siteURL").value;
-
-jQuery.getJSON(adress, function(data){
-    var output="<ul>";
-    for (var i in data.name) {
-        output+="<li>" + data.name[i]+"</li>";
-    }
-
-    output+="</ul>";
-    document.getElementById("out").innerHTML=output;
-});
-};
-function getJN()
-{
-    var request ="https://github.com/Contrkultya/nardy_MOORA/blob/master/pedofile.json";
-    var file = new XMLHttpRequest();
-    file.open('GET', request);
-    file.responseType='json';
-    file.send();
-    file.onload=function(){
-        var bivshaya = file.response;
-        populateHeader(bivshaya);
-        showBivshaya(bivshaya);
-    }
-    function populateHeader(walle){
-        document.getElementById("out").innerHTML=name;
-    }
-
-
-}
-
-var boolExile = false, boolFile = false;
-function file_or_exile_Checker(X){
-    if (X == 1)
-    {
-        boolFile = true;
-        boolExile = false;
-        document.getElementById('siteURL').value = '';
-    }
-    else 
-    {
-        boolExile = true;
-        boolFile = false;
-        document.getElementById('fileLocation').value = '';
-    }
-
-}
-function file_or_exile(){
-    if (boolFile)
-    {
-        //do some method for File
-    }
-    else if (boolExile)
-    {
-        //do some method for Exile
-    }
-}
-
 // working function
 
 function checkMother() {
     var mydata;
-    if (document.getElementById('cock').value !== "") mydata = document.getElementById('cock').value; else
+    if (document.getElementById('json_area').value != "") 
+        mydata = document.getElementById('json_area').value; 
+    else
         mydata = '[{ "name": "Доска 1", "children": [{ "name": "Список задач 1.1","children": [{ "name": "Задача 1.1.1" },{ "name": "Задача 1.1.2" }]},{"name": "Список задач 1.2","children": [{ "name": "Задача 1.2.1" },{ "name": "Задача 1.2.2" }]},{"name": "Список задач 1.3"}]},{"name": "Доска 2"}]';
     var dat = JSON.parse(mydata);
 

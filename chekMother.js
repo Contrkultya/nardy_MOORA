0d4dbdae56d0b7b6c61053bfb569add38f8891fe
Eugene Pokidko <42346779+Pievy@users.noreply.github.com>
2019-03-09 19:02:46 +0500

    Готовость дз на прошлый четверг
    Идеально, кроме вложенности, дальше 3 уровня вложенности не идёт

--- chekMother.js
+++ chekMother.js
@@ -1,14 +1,10 @@
 // working function
 
 function checkMother() {
-    var mydata;
-    if (document.getElementById('json_area').value != "") 
-        mydata = document.getElementById('json_area').value; 
-    else
-        mydata = '[{ "name": "Доска 1", "children": [{ "name": "Список задач 1.1","children": [{ "name": "Задача 1.1.1" },{ "name": "Задача 1.1.2" }]},{"name": "Список задач 1.2","children": [{ "name": "Задача 1.2.1" },{ "name": "Задача 1.2.2" }]},{"name": "Список задач 1.3"}]},{"name": "Доска 2"}]';
-    var dat = JSON.parse(mydata);
-
-
+    var dat = '[{ "name": "Доска 1", "children": [{ "name": "Список задач 1.1","children": [{ "name": "Задача 1.1.1" },{ "name": "Задача 1.1.2" }]},{"name": "Список задач 1.2","children": [{ "name": "Задача 1.2.1" },{ "name": "Задача 1.2.2" }]},{"name": "Список задач 1.3"}]},{"name": "Доска 2"}]';
+    if (document.getElementById('json_area').value != '')
+        dat = document.getElementById('json_area').value;
+    dat = JSON.parse(dat);
     var html = '<ul>';
     for (var k in dat) {
         html = html + '<li>' + dat[k].name + '</li>';

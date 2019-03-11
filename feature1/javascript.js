c6f2d3ac4ef264f151ebf50e54fb650303e4fe1c
Eugene Pokidko <42346779+Pievy@users.noreply.github.com>
2019-03-11 15:21:02 +0500

    feature1: Документация js'a
    • Добавлена документация
    • Оптимизирован код:  удалены повторения и изменена логика
    • Введено отслеживание ввода в textarea

--- feature1/javascript.js
+++ feature1/javascript.js
@@ -1,43 +1,38 @@
+/*
+    * Функция, обрабатывающая JSON 
+  * Проверяет, из «index.html», textarea (12 строка) на наличие ввода и обрабатывает полученный JSON;
+  * @param {bool} inputArea — передаёт был ли ввод в textarea;
+  * @param {string} data — хранит в себе исходный текст, потом в итоге спарсированный JSON;
+  * @param {string} html — хранит в себе вывод на html-страницу;
+*/
 function checkMother() {
-  if (document.getElementById('jsonArea').value == '') {
-    var date = '[{ "name": "Доска 1", "children": [{ "name": "Список задач 1.1","children": [{ "name": "Задача 1.1.1" },{ "name": "Задача 1.1.2" }]},{"name": "Список задач 1.2","children": [{ "name": "Задача 1.2.1" },{ "name": "Задача 1.2.2" }]},{"name": "Список задач 1.3"}]},{"name": "Доска 2"}]';
-    dat = JSON.parse(date);
-    var html = "";
-    function rec(data) {
-      html += "<ul>";
-      data.forEach(element => {
-        html += "<li>";
-        html += element.name;
-        if (element.children) {
-          rec(element.children);
-        }
-        html += "</li>";
-      });
-      html += "</ul>";
-      return html;
-    }
-    cool = rec(dat);
-    document.getElementById('out').innerHTML = cool;
+  var inputArea = false;
+  var data = '[{ "name": "Доска 1", "children": [{ "name": "Список задач 1.1","children": [{ "name": "Задача 1.1.1" },{ "name": "Задача 1.1.2" }]},{"name": "Список задач 1.2","children": [{ "name": "Задача 1.2.1" },{ "name": "Задача 1.2.2" }]},{"name": "Список задач 1.3"}]},{"name": "Доска 2"}]';
+  if (document.getElementById('jsonArea').value != ''){
+    data = document.getElementById('jsonArea').value;
+    inputArea = true;
   }
-  else {
-    var date = document.getElementById('jsonArea').value;
-    dat = JSON.parse(date);
-    var html = "";
-    function rec(data) {
-      html += "<ul>";
-      data.forEach(element => {
-        html += "<li>";
+  data = JSON.parse(data);
+  var html = "";
+  /*
+      * Рекурсивная функция
+    * Получает спарсированный JSON;
+    * Возвращает {string} html;
+  */
+  function rec(data) {
+    html += "<ul>";
+    data.forEach(element => {
+      html += "<li>";
+      if (inputArea)
         html += element;
-        if (element.children) {
-          rec(element.children);
-        }
-        html += "</li>";
-      });
-      html += "</ul>";
-      return html;
-    }
-    cool = rec(dat);
-    document.getElementById('out').innerHTML = cool;
+      else html += element.name;
+      if (element.children)
+        rec(element.children);
+      html += "</li>"
+    });
+    html += "</ul>";
+    return html;
   }
-  document.getElementById("out").style.display = "block";
+  document.getElementById('out').innerHTML = rec(data);
+  document.getElementById('out').style.display = "block";
 }

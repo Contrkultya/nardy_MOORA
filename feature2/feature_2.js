c4a2f83f0ae2a8b125c20a8e8f52f220f20f4505
Timofey <48101066+Mita57@users.noreply.github.com>
2019-03-11 16:14:25 +0500

    Update feature_2.js
    адекватный массив чекбоксов

--- feature2/feature_2.js
+++ feature2/feature_2.js
@@ -1,76 +1,74 @@
-var number=0;
-var object=[];
+var number = 0;
+var object = [];
 var obj = {
-    removed : false,
-    name:"",
-    value:"",
-    delete:function(){
-        this.removed=true;
+    removed: false,
+    name: "",
+    value: "",
+    delete: function () {
+        this.removed = true;
     },
-    read:function(){
+    read: function () {
         var outism;
-        outism+=(this.name) + " ";
-        outism+=(this.value)+ " ";
-        outism+=(this.removed)+ " ";
-        document.getElementById('outInfo').value=outism;
+        outism += (this.name) + " ";
+        outism += (this.value) + " ";
+        outism += (this.removed) + " ";
+        document.getElementById('outInfo').value = outism;
 
     },
-    readkey:function(key){
-        document.getElementById('outInfo').value=(this.key);
+    readkey: function (key) {
+        document.getElementById('outInfo').value = (this.key);
     },
-    update:function(key){
-        updObj=document.getElementById('chng_val').value;
+    update: function (key) {
+        updObj = document.getElementById('chng_val').value;
     }
 
 
 }
 
 
-function objBuilder(){
+function objBuilder() {
     object[number] = Object.create(obj);
     var setname = document.getElementById('name').value;
     var setvalue = document.getElementById('value').value;
-    object[number].removed=false;
-    object[number].name=setname;
-    object[number].value=setvalue;
+    object[number].removed = false;
+    object[number].name = setname;
+    object[number].value = setvalue;
     number++;
     printObj();
 }
 
-function printObj (){ 
-    var output="";
-    for (i=0; i<object.length;i++)
-    {   
-        if(object[i].removed==false) output+=('<input type="checkbox" id="chck'+i+'">'+object[i].name+ " <p></p>\n");
+function printObj() {
+    var output = "";
+    for (i = 0; i < object.length; i++) {
+        if (object[i].removed == false) output += ('<input type="checkbox" name="chck">' + object[i].name + " <p></p>\n");
     }
-    document.getElementById('createdObjects').innerHTML=output;
+    document.getElementById('createdObjects').innerHTML = output;
 }
 
-function removeKebab(){
-    for(i=0; i<object.length;i++){
-    if(document.getElementById(chck+"i").checked==true) object[i].removed=true;}
-}
-function read(){
-    var sel;
-    for(i=0; i<object.length;i++)
-    {
-    if(document.getElementById(chck+"i").checked==true) sel=i; break;
-    }
-    switch(document.getElementsByName("key").value){
-        case("name"): object[sel].readkey(name);
-        case("value"): object[sel].readkey(value);
-        case("everything"): object[sel].read();
+function removeKebab() {
+    for (i = 0; i < chck.length; i++) {
+        if (document.chck[i].checked == true) object[i].removed = true;
     }
 }
-function upd(){
+function read() {
     var sel;
-    for(i=0; i<object.length;i++)
-    {
-    if(document.getElementById(chck+"i").checked==true) sel=i; break;
+    for (i = 0; i < object.length; i++) {
+        if (document.chck[i].checked == true) sel = i; break;
     }
-    switch(document.getElementsByName(change_key).value){
-        case("name"): object[sel].update(name);
-        case("value"): object[sel].update(value);
+    switch (document.getElementsByName("key").value) {
+        case ("name"): object[sel].readkey(name);
+        case ("value"): object[sel].readkey(value);
+        case ("everything"): object[sel].read();
+    }
+}
+function upd() {
+    var sel;
+    for (i = 0; i < object.length; i++) {
+        if (document.chck[i].checked == true) sel = i; break;
+    }
+    switch (document.getElementsByName(change_key).value) {
+        case ("name"): object[sel].update(name);
+        case ("value"): object[sel].update(value);
     }
 
 }

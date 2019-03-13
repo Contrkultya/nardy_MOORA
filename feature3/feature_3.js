fd64d98a4e9070a04659bc03cafddf1ab5909331
Eugene Pokidko <42346779+Pievy@users.noreply.github.com>
2019-03-13 21:53:45 +0500

    Update feature_3.js

--- feature3/feature_3.js
+++ feature3/feature_3.js
@@ -78,6 +78,21 @@ class obj {
     };
 }
 
+var number = 0;
+var object = [];
+function objBuilder_file(data) {
+    for (let i = 0; i < data.length; i++) {
+        let setid = data[i].id;
+        let setparent = data[i].parent;
+        let setname = data[i].name;
+        let hasChildren = data[i].hasChildren;
+        let setremoved = data[i].removed;
+        object[number] = new obj(setid, setparent, setname, hasChildren, setremoved);
+        number++;
+        printObj_file();
+    }
+}
+
 var data = '[{ "id": 1,"name": "Доска 1","hasChildren": true},{"id": 2,"parent": 1,"name": "Список задач 1.1","hasChildren": true},{ "id": 3,"parent": 2,"name": "Задача 1.1.1" },{ "id": 4,"parent": 2,"name": "Задача 1.1.2" },{"id": 5,"parent": 1,"name": "Список задач 1.2","hasChildren": true},{ "id": 6,"parent": 5,"name": "Задача 1.2.1" },{ "id": 7,"parent": 5,"name": "Задача 1.2.2" },{"id": 8,"parent": 1,"name": "Список задач 1.3"},{"id": 9,"name": "Доска 2"}]';
 
 //onLoad

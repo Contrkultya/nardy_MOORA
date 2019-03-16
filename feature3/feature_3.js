8bc735e1d51bc8346176b3ec264b06c686e715d8
Eugene Pokidko <42346779+Pievy@users.noreply.github.com>
2019-03-16 21:20:38 +0500

    narDycummentation 1.2

--- feature3/feature_3.js
+++ feature3/feature_3.js
@@ -1,14 +1,12 @@
-//@global [array] keys — массив ключей;
-var keys = [ 'id', 'parent', 'name', 'hasChildren', 'remove'];
 /*
         * Класс obj — основная база для взаимодействия с объектами. Присутствует конструктор, методы чтения, обновления, удаления.
-    @constructor:
+    * @constructor:
     * Создаёт объект из полученных данных:
-        @param {string/number} id — ключ-идентификатор элемента;
-        @param {string/number} parent — ключ-идентификатор родителя; 
-        @param {string} name — ключ-имя элемента;
-        @param {bool} hasChildren — ключ-признак на наличие дочерних элементов;
-        @param {bool} removed — ключ-признак удаления элемента;
+        * @param {string|number} id — ключ-идентификатор элемента;
+        * @param {string|number} parent — ключ-идентификатор родителя; 
+        * @param {string} name — ключ-имя элемента;
+        * @param {bool} hasChildren — ключ-признак на наличие дочерних элементов;
+        * @param {bool} removed — ключ-признак удаления элемента;
 */
 class obj {
     constructor(id, parent, name, hasChildren) {
@@ -43,7 +41,7 @@ class obj {
             }
         }
     }
-    // * Метод, который получает обновлённый объект и изменяет ключи объекта.
+    // * Метод, который получает обновлённый объект и изменяет ключи на ключи полученного объекта.
     update(updObj){
         for (let i = 0; i < 5; i++) {
             this.keys[i] = updObj.keys[i];
@@ -54,9 +52,25 @@ class obj {
         this.removed = true;
     }
 }
-
+/*
+        * Глобальные переменные для вывода.
+    * @global {number} number — число элементов;
+    * @global {array} object — массив объектов;
+    * @global [array] keys — массив ключей;
+*/
 var number = 0;
 var object = [];
+var keys = [ 'id', 'parent', 'name', 'hasChildren', 'remove' ];
+/*
+        * Функция, строящая объект.
+    * @param {json} data — передаваемый в функцию JSON; 
+    * @param {number|string} setid — хранит в себе введённый идентификатор;
+    * @param {number|string} setparent — хранит в себе введённое значение;
+    * @param {string} setname — хранит в себе введённое имя;
+    * @param {bool} hasChildren — хранит в себе информацию о наличии дочерних элементов;
+    * @param {bool} setremoved — хранит в себе информацию о наличии «удаления»;
+    * @global {number} number — переменная-счётчик количества объектов;
+*/
 function objBuilder_file(data) {
     for (let i = 0; i < data.length; i++) {
         let setid = data[i].id;
@@ -68,29 +82,32 @@ function objBuilder_file(data) {
         number++;
     }
 }
-
+/*
+        * Демо-данные
+    * @global {string} -> {json} data — хранение данных;
+*/
 var data = '[{ "id": 1,"name": "Доска 1","hasChildren": true},{"id": 2,"parent": 1,"name": "Список задач 1.1","hasChildren": true},{ "id": 3,"parent": 2,"name": "Задача 1.1.1" },{ "id": 4,"parent": 2,"name": "Задача 1.1.2" },{"id": 5,"parent": 1,"name": "Список задач 1.2","hasChildren": true},{ "id": 6,"parent": 5,"name": "Задача 1.2.1" },{ "id": 7,"parent": 5,"name": "Задача 1.2.2" },{"id": 8,"parent": 1,"name": "Список задач 1.3"},{"id": 9,"name": "Доска 2"}]';
 data = JSON.parse(data);
-var end="";
+/*
+        * Функция-коллбек 
+*/
 function getChildren(callback) {
     setTimeout(function(){
         callback(id);   
         for(i = 0; i<mas.length;i++)
         { 
-            document.getElementById(id).innerHTML+="<li id='"+(mas[i].id+"li")+"'>"+mas[i].name+" <button onClick=render('"+mas[i].id+"li"+"')>render</button>"+"</li>";
+            document.getElementById(id).innerHTML+="<li id='"+(mas[i].id+"li")+"'>"+mas[i].name+" <button onClick=render('"+mas[i].id+"li"+"')>Изменить стиль</button>"+"</li>";
             if(mas[i].hasChildren == true)
             {
-                document.getElementById(id).innerHTML+="<button onClick=res("+(mas[i].id-1)+")>load</button><ul><div id='"+(mas[i].id-1)+"'></ul>";
+                document.getElementById(id).innerHTML+="<button onClick=res("+(mas[i].id-1)+")>Открыть</button><ul><div id='"+(mas[i].id-1)+"'></ul>";
             }
         }
     }, 1000);
     
 }
 function loadChildren(id, callback) {
-    for(i=id;i<data.length;i++)
-    {
-        if(data[i].hasChildren==true)
-        {
+    for (i = id; i<data.length; i++) {
+        if (data[i].hasChildren == true) {
             modelBuilder(data[i].id);
         }
     }
@@ -98,41 +115,39 @@ function loadChildren(id, callback) {
 function modelBuilder(id) {
     mas=[];
     j=0;
-    for(i = id; i<data.length;i++)
-    {
-        if(data[i].parent == id)
-        {
-            mas[j]=data[i];
+    for (i = id; i<data.length;i++) {
+        if (data[i].parent == id) {
+            mas[j] = data[i];
             j++;
         }
     }
 }
 function load() {
     document.getElementById("result").innerHTML +="<ul>";
-    for(i=0;i<data.length; i++)
+    for (i=0;i<data.length; i++)
     {
-        if(data[i].parent == undefined)
-        {
-            document.getElementById("result").innerHTML +="<li id='"+(data[i].id+"li")+"'>"+data[i].name+" <button onClick=render('"+(data[i].id+"li")+"')>render</button></li>";
-            if(data[i].hasChildren==true)
-            {
-                document.getElementById("result").innerHTML+="<button onClick=res("+i+")>load</button><ul><div id='"+i+"'></ul>";
+        if (data[i].parent == undefined) {
+            document.getElementById("result").innerHTML +="<li id='"+(data[i].id+"li")+"'>"+data[i].name+" <button onClick=render('"+(data[i].id+"li")+"')>Изменить стиль</button></li>";
+            if (data[i].hasChildren==true) {
+                document.getElementById("result").innerHTML+="<button onClick=res("+i+")>Открыть</button><ul><div id='"+i+"'></ul>";
             }
         }
     }
 }
- function res(m) {
-    if(document.getElementById(m).innerHTML=="")
-    {
-        id=m;
+function res(m) {
+    if(document.getElementById(m).innerHTML == "") {
+        id = m;
         getChildren(loadChildren);
     }
-    else if(document.getElementById(m).innerHTML!="")
-    {
-        document.getElementById(m).innerHTML="";
+    else if(document.getElementById(m).innerHTML != "") {
+        document.getElementById(m).innerHTML = "";
     }
 }
+/*
+        * Функция-управлятор — благодаря ей, пользователь может изменять стили определённого элемента в списке.
+    * @param {string} stl — переменная изменяющая стиль;
+*/
 function render(x) {
-    stl=prompt("Вводите стиль:");
-    document.getElementById(x).style=end+stl;
+    stl = prompt("Вводите стиль:");
+    document.getElementById(x).style = stl;
 }

f7e095548d976828c51f285ce23c324fbfe13bb0
Timofey <48101066+Mita57@users.noreply.github.com>
2019-04-12 18:38:06 +0500

    sdghjkl;

--- feature9/index.js
+++ feature9/index.js
@@ -185,6 +185,10 @@ var obj = function () {
     return obj;
 }();
 
+function close(id){
+    document.getElementById(id).innerHTML="";
+}
+
 var bankLogic = {
     data: (_JSON$parse = JSON.parse('[{ "id": 1,"name": "Доска 1","hasChildren": true, "done": false, "removed": false},{"id": 2,"parent": 1,"name": "Список задач 1.1","hasChildren": true, "done": false, "removed": false, "description": "Сделать removeKebab 2.0"},{ "id": 3,"parent": 2,"name": "Задача 1.1.1", "done": false, "removed": false, "description": "Сделать removeAutism"},{ "id": 4,"parent": 2,"name": "Задача 1.1.2", "done": true, "removed": false, "description": "Сделать vue"},{ "id": 10,"parent": 2,"name": "Задача 1.1.3", "done": false, "removed": false},{"id": 5,"parent": 1,"name": "Список задач 1.2","hasChildren": true, "done": false, "removed": false},{ "id": 6,"parent": 5,"name": "Задача 1.2.1" , "done": false, "removed": false},{ "id": 7,"parent": 5,"name": "Задача 1.2.2", "done": false, "removed": false },{"id": 8,"parent": 1,"name": "Список задач 1.3", "done": false, "removed": false, "description": "Радоваться жизни"},{"id": 9,"name": "Доска 2", "done": false, "removed": false}]'), _JSON$parse2 = _toArray(_JSON$parse), _JSON$parse),
     counter: 0,
@@ -254,7 +258,7 @@ function load() {
             "<button onClick='render(" + bankLogic.data[i].id + ")'>Изменить стиль</button></div>"; /** Кнопка изменения стиля */
             /** Если иммеет дочерние элементы, добавляет кнопку получения элементов. */
             if (bankLogic.data[i].hasChildren == true) {
-                document.getElementById(bankLogic.data[i].id + "d").innerHTML += "<button onClick='res(" + bankLogic.data[i].id + ")'>Открыть</button><div style='margin:0 auto;' id='" + bankLogic.data[i].id + "_p_" + "'></div>";
+                document.getElementById(bankLogic.data[i].id + "d").innerHTML += "<button onClick='res(" + bankLogic.data[i].id + ")'>Открыть</button><button onClick='close("+'"' + bankLogic.data[i].id +'_p_'+'"'+ ")'>Закрыть</button><div style='margin:0 auto;' id='" + bankLogic.data[i].id + "_p_" + "'></div>";
             }
         }
     }
@@ -310,7 +314,7 @@ function getChildren() {
             "<button onClick='render(" + object[i].id + ")'>Изменить стиль</button>" + "</li>" + /** Кнопка изменения стиля */
             "<div id='" + object[i].id+ "description' class='descriptionLogic' style='display: none;'>" + object[i].description + "</div></div>"; /** Описание */
             if (object[i].hasChildren == true) {
-                document.getElementById("p_" + object[i].id).innerHTML += "<button onClick='res(" + object[i].id + ")'>Открыть</button><div style='margin:0 auto;margin-bottom:30px;' id='" + (object[i].id + "_p_") + "'></div><hr>";
+                document.getElementById("p_" + object[i].id).innerHTML += "<button onClick='res(" + object[i].id + ")'>Открыть</button><button onClick='close("+'"' + object[i].id+'"'+ ")'>Закрыть</button><div style='margin:0 auto;margin-bottom:30px;' id='" + (object[i].id + "_p_") + "'></div><hr>";
             }
         }
     }, 1000);

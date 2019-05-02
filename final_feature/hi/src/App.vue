fb1faa8bd5e8a2a3c0a957d599b0a9a6675fa69d
Eugene Pokidko <42346779+Pievy@users.noreply.github.com>
2019-05-02 18:32:26 +0500

    Hi i HZ how it works

---
+++ final_feature/hi/src/App.vue
@@ -0,0 +1,114 @@
+<template>
+  <v-app>
+    <v-toolbar app style="background-color: indigo">
+      <v-toolbar-title class="headline text-uppercase" style="color: white">
+        <span>NARDY MOORA </span>
+        <span class="font-weight-light"> MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar >
+
+    <v-content id="boardlist">
+    <v-layout style="height:100%;">
+       <v-navigation-drawer>
+        <v-toolbar flat>
+        <v-list>
+          <v-list-tile>
+            <v-list-tile-title class="title">
+              Список досок
+            </v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-toolbar>
+  
+      <v-divider></v-divider>
+  
+      <v-list dense class="pt-0">
+        <v-list-tile
+          v-for="obj in desks"
+          :key="obj.id"
+          @click=""
+          
+        >
+          <v-list-tile-action>
+            <v-icon
+             v-for="icon in items"
+          :key="icon.icon"
+          @click=""
+            >{{icon.icon}}</v-icon>
+          </v-list-tile-action>
+  
+          <v-list-tile-content>
+            <v-list-tile-title>{{ obj.name }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-flex xs12 sm6 offset-sm3>
+      <v-card
+       v-for="obj in childrens"
+          :key="obj.id"
+          @click=""
+          style="margin-top:10px;"
+      >
+        
+        <v-card-title primary-title>
+          <div>
+            <div class="headline">{{obj.name}}</div>
+            <span class="grey--text"></span>
+          </div>
+        </v-card-title>
+
+        <v-slide-y-transition>
+          <v-card-text>
+            {{obj.description}}
+          </v-card-text>
+        </v-slide-y-transition>
+
+               <v-card-actions>
+          <v-btn flat color="purple">Завершить</v-btn>
+          <v-btn flat color="blue">Удалить</v-btn>
+          <v-spacer></v-spacer>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+    </v-content>
+  </v-app>
+</template>
+<script>
+export default {
+  data(){
+       return {
+      items: [
+        { icon: 'dashboard' }
+      ],
+      right: null
+    }
+  },
+  computed:{
+    desks(){
+      return this.$store.getters.getdesk;
+    },
+    childrens(){
+      return this.$store.getters.getChildren;
+    }
+  }
+
+}
+
+</script>
+
+<style>
+
+</style>
+
+
+
+
+
+
+
+
+
---
+++ final_feature/hi/src/store.js
@@ -0,0 +1,59 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export const store = new Vuex.Store({
+  state:{
+    todos: [
+    { "id": 1,"name": "Доска 1", "parent": 0,"hasChildren": true, "done": false, "removed": false},
+    { "id": 2,"parent": 1,"name": "Список задач 1.1","hasChildren": true, "done": false, "removed": false, "description": "Сделать removeKebab 2.0"},
+    { "id": 3,"parent": 2,"name": "Задача 1.1.1", "done": false, "removed": false, "description": "Сделать removeAutism"},
+    { "id": 4,"parent": 2,"name": "Задача 1.1.2", "done": true, "removed": false, "description": "Сделать vue"},
+    { "id": 9,"parent": 2,"name": "Задача 1.1.3", "done": false, "removed": false, "description": "Заработать тут чекбох"},
+    { "id": 5,"parent": 1,"name": "Список задач 1.2","hasChildren": true, "done": false, "removed": false},
+    { "id": 6,"parent": 5,"name": "Задача 1.2.1" , "done": false, "removed": false},
+    { "id": 7,"parent": 5,"name": "Задача 1.2.2", "done": false, "removed": false },
+    { "id": 8,"parent": 1,"name": "Список задач 1.3", "done": false, "removed": false, "description": "Радоваться жизни"},
+    { "id": 10,"name": "Доска 2","parent": 0, "done": false, "removed": false}
+    ],
+    currentId: 0
+   },
+getters: {
+ getTodoById: state => id => {
+ return state.todos.find(todo => todo.id === id);
+},
+todosCount: state => {
+ return state.todos.length;
+},
+currentId: state => {
+         return state.currentId;
+ },
+ todos: state => {
+   return state.todos;
+ },
+ getdesk: state =>{
+return state.todos.filter(todos=>todos.parent === 0)
+ },
+ getChildren: state =>{
+  return state.todos.filter(todos=>todos.parent === 1)
+   }
+     
+   //прописать методы взаимодействия 
+},
+mutations: {
+ setCurrentId(state, id){
+     state.currentId = id;
+   //прописать мутации и коммиты
+ },
+ newNote(state, note){
+     state.todos.push(note);
+ },
+ delNote(state, id){
+     state.todos.splice(state.todos.find(todo => todo.id === id), 1);
+ }
+},
+actions: {
+   //добавить асинхронности
+},
+})

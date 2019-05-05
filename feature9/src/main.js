cd60f4aed8ed8f4cfe166f4cac9aa91834fb3515
Igor Krupkin <48021172+Cuberian@users.noreply.github.com>
2019-05-05 16:53:58 +0500

    dud

---
+++ feature9/src/main.js
@@ -0,0 +1,10 @@
+import './plugins/vuetify'
+import './plugins/vuetify'
+import './plugins/vuetify'
+import './plugins/vuetify'
+import App from "./App.vue";
+new Vue({
+    el: '#app',
+    components: { App },
+    template: '<App/>'
+  })

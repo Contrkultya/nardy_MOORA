85edbb8c3e86a78b725805f49e938de86e606bd8
Eugene Pokidko <42346779+Pievy@users.noreply.github.com>
2019-05-05 19:35:14 +0500

    some beauty

--- FINAL_BUTNOTFINAL/my-app/src/App.vue
+++ FINAL_BUTNOTFINAL/my-app/src/App.vue
@@ -9,93 +9,93 @@
     </v-toolbar >
 
     <v-content id="boardlist">
-    <v-layout style="height:100%;">
-       <v-navigation-drawer style="z-index:0;">
-        <v-toolbar flat>
-        <v-list>
-          <v-list-tile>
-            <v-list-tile-title class="title">
-              Список досок
-            </v-list-tile-title>
-          </v-list-tile>
-        </v-list>
-      </v-toolbar>
-  
-      <v-divider></v-divider>
-  
-      <v-list dense class="pt-0">
-        <v-list-tile
-          v-for="obj in desks"
-          :key="obj.id"
-          @click="show=true"
-        >
-          <v-list-tile-action>
-            <v-icon
-             v-for="icon in items"
-          :key="icon.icon"
-          @click=""
-            >{{icon.icon}}</v-icon>
-          </v-list-tile-action>
-  
-          <v-list-tile-content>
-            <v-list-tile-title>{{ obj.name }}</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-       <v-btn flat color="purple">Добавить</v-btn> 
-      </v-list>
-    </v-navigation-drawer>
+      <v-layout style="height:100%;">
+        <v-navigation-drawer style="z-index:0;">
+            <v-toolbar flat>
+              <v-list>
+                <v-list-tile>
+                  <v-list-tile-title class="title">
+                    Список досок
+                  </v-list-tile-title>
+                </v-list-tile>
+              </v-list>
+            </v-toolbar>
 
-    <v-flex v-if="show==true" style="align-center justify-right row/">
-      <v-card
-       v-for="(obj, id) in childrens"
-          :key="id"
-          @click=""
-          :style=""
-          style="margin-top:10px; width:300px;margin-left:10px"
-      >
-        
-        <v-card-title primary-title>
-          <div>
-            <div class="headline">{{obj.name}}</div>
-            <span class="grey--text"></span>
-          </div>
-        </v-card-title>
+          <v-divider></v-divider>
+  
+          <v-list dense class="pt-0">
+            <v-list-tile
+              v-for="obj in desks"
+              :key="obj.id"
+              @click="show=true"
+              >
+              <v-list-tile-action>
+                <v-icon
+                v-for="icon in items"
+                :key="icon.icon"
+                @click=""
+                >{{icon.icon}}</v-icon>
+              </v-list-tile-action>
+      
+              <v-list-tile-content>
+                <v-list-tile-title>{{ obj.name }}</v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+            <v-btn flat color="purple">Добавить</v-btn> 
+          </v-list>
+        </v-navigation-drawer>
 
-        <v-slide-y-transition>
-          <v-card-text>
-            {{obj.description}}
-          </v-card-text>
-        </v-slide-y-transition>
-
-               <v-card-actions>
-          <v-btn flat color="green"
-          @click="status=!status">Завершить</v-btn>
-          <v-btn flat color="red" 
-          @click="del(obj.id)">Удалить</v-btn>
-          <v-spacer></v-spacer>
-        </v-card-actions>
-      </v-card>
-     <v-btn
-              color="#ef5350"
-              dark
-              big7
-              absolute
-              top
-              right
-              fab
-              @click=""
+        <v-flex v-if="show==true" style="align-center justify-right row/">
+          <v-card
+            v-for="(obj, id) in childrens"
+            :key="id"
+            @click=""
+            :style=""
+            style="margin-top:10px; width:300px;margin-left:10px"
             >
-              <v-icon>add</v-icon>
-            </v-btn>
-    </v-flex>
-  </v-layout>
+            
+            <v-card-title primary-title>
+              <div>
+                <div class="headline">{{obj.name}}</div>
+                <span class="grey--text"></span>
+              </div>
+            </v-card-title>
+
+            <v-slide-y-transition>
+              <v-card-text>
+                {{obj.description}}
+              </v-card-text>
+            </v-slide-y-transition>
+
+            <v-card-actions>
+              <v-btn flat color="green"
+              @click="status=!status">Завершить</v-btn>
+              <v-btn flat color="red" 
+              @click="del(obj.id)">Удалить</v-btn>
+              <v-spacer></v-spacer>
+            </v-card-actions>
+          </v-card>
+          <v-btn
+            color="#ef5350"
+            dark
+            big7
+            absolute
+            top
+            right
+            fab
+            @click=""
+            >
+            <v-icon>add</v-icon>
+          </v-btn>
+        </v-flex>
+      </v-layout>
     </v-content>
   </v-app>
 </template>
 <script>
 export default {
-  data(){
-       return {
+  data() {
+    return {
       items: [
         { icon: 'dashboard' }
       ],
@@ -105,32 +105,28 @@ export default {
     }
   },
   computed:{
-    desks(){
-      
+    desks() {
       return this.$store.getters.getdesk;
     },
-    childrens()
-    {
+    childrens() {
       return this.$store.getters.getChildren;
     },
-    object()
-    {
+    object() {
       return this.$store.getters.todos;
     },
-    count()
-    {
+    count() {
       return this.$store.getters.todosCount;
     }
-    },
-    methods:{
-        del:function(id){
+  },
+  methods: {
+    del:function(id) {
       this.$store.dispatch('del',id);
-      },
-        add:function(id){
-           this.$store.dispatch('add',id);
-      },
-    }
+    },
+    add:function(id) {
+      this.$store.dispatch('add',id);
+    },
   }
+}
 
 
 </script>
--- FINAL_BUTNOTFINAL/my-app/src/store.js
+++ FINAL_BUTNOTFINAL/my-app/src/store.js
@@ -4,7 +4,7 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export const store = new Vuex.Store({
-  state:{
+  state: {
     todos: [
     { "id": 1,"name": "Доска 1", "parent": 0,"hasChildren": true, "done": true, "removed": false},
     { "id": 2,"parent": 1,"name": "Список задач 1.1","hasChildren": true, "done": true, "removed": false, "description": "Сделать removeKebab 2.0"},
@@ -15,50 +15,50 @@ export const store = new Vuex.Store({
     { "id": 6,"parent": 5,"name": "Задача 1.2.1" , "done": false, "removed": false},
     { "id": 7,"parent": 5,"name": "Задача 1.2.2", "done": false, "removed": false },
     { "id": 8,"parent": 1,"name": "Список задач 1.3", "done": false, "removed": false, "description": "Радоваться жизни"},
-    { "id": 10,"name": "Доска 2","parent": 0, "done": false, "removed": false}
+    { "id": 10,"parent": 0,"name": "Доска 2", "done": false, "removed": false}
     ],
     currentId: 0
    },
-getters: {
- getTodoById: state => id => {
- return state.todos.find(todo => todo.id === id);
-},
-todosCount: state => {
- return state.todos.length;
-},
-currentId: state => {
-         return state.currentId;
- },
- todos: state => {
-   return state.todos;
- },
- getdesk: state =>{
-return state.todos.filter(todos=>todos.parent === 0)
- },
- getChildren: state =>{
-  return state.todos.filter(todos=>todos.parent === 1)
-   }
+   getters: {
+    getTodoById: state => id => {
+       return state.todos.find(todo => todo.id === id);
+      },
+    todosCount: state => {
+      return state.todos.length;
+    },
+    currentId: state => {
+      return state.currentId;
+    },
+    todos: state => {
+      return state.todos;
+    },
+    getdesk: state => {
+      return state.todos.filter(todos=>todos.parent === 0)
+    },
+    getChildren: state => {
+      return state.todos.filter(todos=>todos.parent === 1)
+    }
      
    //прописать методы взаимодействия 
-},
-mutations: {
- setCurrentId(state, id){
-     state.currentId = id;
-   //прописать мутации и коммиты
- },
- newNote(state, note){
-     state.todos.push(note);
- },
- delNote(state, id){
-     state.todos.splice(state.todos.findIndex(todo => todo.id === id), 1);
- }
-},
-actions: {
-  del ({commit}, id) {         
-          commit('delNote', id)
   },
-  add ({commit}, id) {         
-    commit('newNote', id)
-}
-},
+  mutations: {
+    setCurrentId(state, id) {
+      state.currentId = id;
+      //прописать мутации и коммиты
+    },
+    newNote(state, note) {
+      state.todos.push(note);
+    },
+    delNote(state, id) {
+      state.todos.splice(state.todos.findIndex(todo => todo.id === id), 1);
+    }
+  },
+  actions: {
+    del ({commit}, id) {
+      commit('delNote', id)
+    },
+    add ({commit}, id) {
+      commit('newNote', id)
+    }
+  },
 })

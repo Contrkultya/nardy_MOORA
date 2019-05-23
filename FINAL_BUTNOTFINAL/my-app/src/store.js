00fb0e4445191aaf4a373e33a4f80274c75a5454
Sergo Skeletonson <40607257+Contrkultya@users.noreply.github.com>
2019-05-23 17:08:25 +0500

    oriflame

--- FINAL_BUTNOTFINAL/my-app/src/store.js
+++ FINAL_BUTNOTFINAL/my-app/src/store.js
@@ -8,7 +8,7 @@ export const store = new Vuex.Store({
     todos: [
     { "id": 1,"name": "Доска 1", "parent": 0,"hasChildren": true, "done": false, "removed": false},
     { "id": 2,"parent": 1,"name": "Список задач 1.1","hasChildren": true,"color":'white', "done": false, "removed": false, "description": "Сделать removeKebab 2.0","selectTasks":false, "tasks":[{ "id": 3,"parent": 2,"name": "Задача 1.1.1", "done": false, "removed": false, "description": "Сделать removeAutism"},{ "id": 4,"parent": 2,"name": "Задача 1.1.2", "done": false, "removed": false, "description": "Сделать vue"},{ "id": 9,"parent": 2,"name": "Задача 1.1.3", "done": false, "removed": false, "description": "Заработать тут чекбох"},]},
-    { "id": 5,"parent": 1,"name": "Список задач 1.2","hasChildren": true, "color":'white', "done": false, "removed": false, "selectTasks":false, "tasks":[ { "id": 6,"parent": 5,"name": "Задача 1.2.1" , "done": false, "removed": false},{ "id": 7,"parent": 5,"name": "Задача 1.2.2", "done": false, "removed": false }]},
+    { "id": 5,"parent": 1,"name": "Список задач 1.2","hasChildren": true, "color":'white', "done": false, "removed": false,"description": "Сделать checkMother 2.0", "selectTasks":false, "tasks":[ { "id": 6,"parent": 5,"name": "Задача 1.2.1" , "done": false, "removed": false},{ "id": 7,"parent": 5,"name": "Задача 1.2.2", "done": false, "removed": false }]},
     { "id": 8,"parent": 1,"name": "Список задач 1.3", "color":'white', "done": false, "removed": false, "selectTasks":false, "description": "Радоваться жизни"},
     { "id": 10,"parent": 0,"name": "Доска 2", "done": false, "removed": false}
     ],
